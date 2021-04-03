0dd0d309a96610304242a1d92ffb324a7c0990ae
Jack McKeown <jackmckeown@Jacks-MacBook-Air-2.local>
2021-04-03 17:30:57 +1000

    finished test class

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ class Menu
 
     def menu_options # Gets valid menu option
         option = gets.chomp.upcase
-        while option != "I" and option != "P" and option != "H" and option != "Q"
+        while option != "I" and option != "P" and option != "Q" and option != "T"
             puts "Invalid Input"
             option = gets.chomp.upcase
         end
@@ -16,10 +16,17 @@ class Menu
         puts "What would you like to do:?"
         puts "(I)nstructions"
         puts "(P)lay"
-        puts "(H)istory and Statisitcs"
+        puts "(T)est"
         puts "(Q)uit"
         input = menu_options()
         return input
+    end
+
+    def instructions 
+        puts "Welcome to Gamblers Education! This app main purpose is to educate gamblers with their probelem."
+        puts "The app contains a test which can help you idenity if you should see an addiction specialist based on your results."
+        puts "This app also contains a gambling simutator which you can view the statuistics and probability behind your punts."
+        puts ""
     end
 end
 
@@ -57,42 +64,122 @@ class Play
         return gamble_money
     end
 
-    def gamble(bank_balance, gamble_money, difficulty_choice)
-        if difficulty_choice == "E"
-            random_number = rand(1..100)
-            puts random_number
-            if random_number >= 40
-                winnings = gamble_money  * 105
-                puts winnings
-                bank = bank_balance + winnings
-                return $bank_balance
-            else
-                bank_balance = bank_balance - gamble_money
-                return bank_balance
-            end
-        end
+    def gamble(bank_balance, ga)
+       
 
     end
     
 
 end
 
+class Test
+    def initialize
+    end
 
-bank_balance = nil
+    def test
+        score = 0
+        puts "Welcome to Gamblers Education's Test."
+        puts "This test is to help indicate if you need to see profestional help for your gambling issues."
+        puts "Answer the question by typing a number between 0-5. 0 being you strongly don't identify and 5 being strongly identify."
+        puts "Do you spend more time or money than intended?"
+        input = gets.strip.to_i
+        score += input
+        puts score
+        puts "Do you argue with family and friends after gambling?"
+        input = gets.strip.to_i
+        score += input
+        puts score
+        puts "After losing at gambling, having an urge to return as soon as possible to win back losses?"
+        input = gets.strip.to_i
+        score += input
+        puts "Do you feel guilty or remorseful about gambling?"
+        input = gets.strip.to_i
+        score += input
+        puts "Do you borrow money or sell assets to gamble?"
+        input = gets.strip.to_i
+        score += input
+        puts "Do you miss work or other commitments to gamble?"
+        input = gets.strip.to_i
+        score += input
+        puts "Do you hide the extent of your gambling?"
+        input = gets.strip.to_i
+        score += input
+        puts score
+        if score <= 5
+            puts "Not critical. See an professtional if gambing abuse symtons worsen"
+        elsif score > 6 and score < 15
+            puts "Your showing symtons of gambling abuse. See attention from a gambling support professtional"
+        else
+            puts "Your showing strong symtoms of gambling addiction. See attention from a gambling support professtional"
+            
+        end
+    
+    end
+
+
+end
+
+
+bank_balance = 5000
 new_menu = Menu.new
 puts "Welome to Gamblers Education!"
-puts "How much money do you want to play with?: "
-bank_balance = gets.strip.to_f
 choice = new_menu.main_menu()
 while choice != "Q"
-
     if choice == "P"
         new_game = Play.new
         difficulty_choice = new_game.difficulty
         gamble_money = new_game.valid_play_money(bank_balance)
-        new_game.gamble(bank_balance, gamble_money, difficulty_choice)
-        puts bank_balance
-        # puts difficulty
+        if difficulty_choice == "E"
+            random_number = rand(1..100)
+            puts random_number
+            if random_number <= 40
+                win = gamble_money * 1.05
+                bank_balance += win
+                puts bank_balance
+            else
+                bank_balance -= gamble_money
+                puts bank_balance
+            end
+        elsif difficulty_choice == "M"
+            random_number = rand(1..100)
+            puts random_number
+            if random_number <= 20
+                win = gamble_money * 1.20
+                bank_balance += win
+                puts bank_balance
+            else
+                bank_balance -= gamble_money
+                puts bank_balance
+            end
+        elsif difficulty_choice == "H"
+            random_number = rand(1..100)
+            puts random_number
+            if random_number <= 10
+                win = gamble_money * 1.40
+                bank_balance += win
+                puts bank_balance
+            else
+                bank_balance -= gamble_money
+                puts bank_balance
+            end
+        elsif difficulty_choice == "V"
+            random_number = rand(1..100)
+            puts random_number
+            if random_number <= 5
+                win = gamble_money * 1.5
+                bank_balance += win
+                puts bank_balance
+            else
+                bank_balance -= gamble_money
+                puts bank_balance
+            end
+        end
+    elsif choice == "I"
+        new_menu.instructions()
+    elsif choice == "T"
+        new_test = Test.new
+        new_test.test()
+    
     end
     choice = new_menu.main_menu()
     
