d7f364eab605180b945b38665cef11f618933e99
Jack McKeown <jackmckeown@Jacks-MacBook-Air-2.local>
2021-04-03 10:34:17 +1000

    started gmable method

--- main.rb
+++ main.rb
@@ -34,7 +34,6 @@ class Play
             option = gets.chomp.upcase
         end
         return option
-
     end
 
     def difficulty 
@@ -45,31 +44,57 @@ class Play
         puts "(V)ery Hard (5% chance of success, 50% earnings)"
         difficulty_input = valid_difficulty()
         return difficulty_input
+        #if difficulty_input == "E"e
+
     end
 
+    def valid_play_money(bank_balance)
+        puts "How much money do you want to gamble?"
+        gamble_money = gets.strip.to_f
+        while gamble_money > bank_balance or bank_balance == 0 
+            puts "You can't gamble more then you have"
+        end
+        return gamble_money
+    end
 
-    # def valid_play_money(bank_balance)
-    #     puts "How much "
-    #     gamble_money
-    #     while 
-    # end
+    def gamble(bank_balance, gamble_money, difficulty_choice)
+        if difficulty_choice == "E"
+            random_number = rand(1..100)
+            puts random_number
+            if random_number >= 40
+                winnings = gamble_money  * 105
+                puts winnings
+                bank = bank_balance + winnings
+                return $bank_balance
+            else
+                bank_balance = bank_balance - gamble_money
+                return bank_balance
+            end
+        end
 
+    end
     
 
 end
 
 
-
-bank_balance = 5000
+bank_balance = nil
 new_menu = Menu.new
 puts "Welome to Gamblers Education!"
+puts "How much money do you want to play with?: "
+bank_balance = gets.strip.to_f
 choice = new_menu.main_menu()
 while choice != "Q"
 
     if choice == "P"
         new_game = Play.new
         difficulty_choice = new_game.difficulty
+        gamble_money = new_game.valid_play_money(bank_balance)
+        new_game.gamble(bank_balance, gamble_money, difficulty_choice)
+        puts bank_balance
         # puts difficulty
     end
     choice = new_menu.main_menu()
+    
+    
 end
