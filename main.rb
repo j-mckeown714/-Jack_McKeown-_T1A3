36b148c32a538ff62eee0e8ec1406db5a9eb2633
Jack McKeown <jackmckeown@Jacks-MacBook-Air-2.local>
2021-04-04 16:49:16 +1000

    added error checking to addiction test

--- main.rb
+++ main.rb
@@ -119,34 +119,41 @@ class Test
     def initialize
     end
 
-    
+    def valid_test_input
+        input = gets.strip.to_i
+        while input != 0 and input != 1 and input != 2 and input != 3 and input != 4 and input != 5
+            puts "Invalid input"
+            input = gets.strip.to_i
+        end
+        return input
+    end
 
 
-    def test
+    def test()
         score = 0
         puts "Welcome to Gamblers Education's Test."
         puts "This test is to help indicate if you need to see profestional help for your gambling issues."
         puts "Answer the question by typing a number between 0-5. 0 being you strongly don't identify and 5 being strongly identify."
         puts "Do you spend more time or money than intended?"
-        input = gets.strip.to_i
+        input = valid_test_input()
         score += input
         puts "Do you argue with family and friends after gambling?"
-        input = gets.strip.to_i
+        input = valid_test_input()
         score += input
         puts "After losing at gambling, having an urge to return as soon as possible to win back losses?"
-        input = gets.strip.to_i
+        input = valid_test_input()
         score += input
         puts "Do you feel guilty or remorseful about gambling?"
-        input = gets.strip.to_i
+        input = valid_test_input()
         score += input
         puts "Do you borrow money or sell assets to gamble?"
-        input = gets.strip.to_i
+        input = valid_test_input()
         score += input
         puts "Do you miss work or other commitments to gamble?"
-        input = gets.strip.to_i
+        input = valid_test_input()
         score += input
         puts "Do you hide the extent of your gambling?"
-        input = gets.strip.to_i
+        input = valid_test_input()
         score += input
         if score <= 5
             puts "Not critical. See an professtional if gambing abuse symtons worsen"
